4309d76a73260e8e7a2ddb44f1f040d5e59c9788
Ray <yhwray-git@yahoo.com>
2020-02-01 01:19:28 +0800

    Assignmenmt 1

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -4,11 +4,15 @@ import redis
 
 
 # fill in the following.
-HOST = "redis-11363.c1.asia-northeast1-1.gce.cloud.redislabs.com"
-PWD = "1nOA0St0I7p9pQqu8HkQ18XqDfnoPeoL"
-PORT = "11363" 
+# HOST = "redis-11363.c1.asia-northeast1-1.gce.cloud.redislabs.com"
+# PWD = "1nOA0St0I7p9pQqu8HkQ18XqDfnoPeoL"
+# PORT = "11363" 
+HOST = "redis-18040.c1.ap-southeast-1-1.ec2.cloud.redislabs.com"
+PWD = "xmiprQJOCm4tKebf8zbudmXt9I99fiRV"
+PORT = "18040" 
 
 redis1 = redis.Redis(host = HOST, password = PWD, port = PORT)
+
 
 while True:
     msg = input("Please enter your query (type 'quit' or 'exit' to end):").strip()
@@ -20,7 +24,14 @@ while True:
 
    
     # Add your code here
+    
+    count = 0
+    if redis1.get(msg):
+        count = int(redis1.get(msg))
+
+    value = 1 + count
+    redis1.set(msg, value)
+
+    print('for '+ str(value) +' times' )
 
 
-
-    print('for X times')
